417a246013e83c0c7c81248f704c460b3305da9f
Maazchogle <110540925+Maazchogle@users.noreply.github.com>
2024-04-25 20:05:51 +0530

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -3,12 +3,7 @@
                                                 Experiment No : 03
                                     AIM : Implementation of DFS Algorithmm
 
-                                                                                                        @Learner: TE-CO
-                                                                                                        Name: Maaz Aslam Chogle 
-                                                                                                        Roll No: 21CO19
-                                                                                                        Batch: 1
-                                                                                                        Academic Year: 2023- 2024
-                                                                                                        Sem - 6
+                                                                                                      
                                                                                                         
 '''
 # Using a Python dictionary to act as an adjacency list
